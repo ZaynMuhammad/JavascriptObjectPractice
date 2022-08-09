aac0a2e20eee0f6e87d9e0047b399d83195abad5
zaynmuhammad <zaynmuhammad4@hotmail.com>
2022-08-09 13:16:58 -0400

    Extract code from averageSomeCoursesPerYear into 2 seperate functions.

--- courses.ts
+++ courses.ts
@@ -22,45 +22,8 @@ interface CourseAvg {
     [key: string]: number
 }
 
-const createCourseAvgObj = (year: string, courseAvgObj: CourseAvg, courseMarksObj: CourseMarks) => {  
-    
-    for (const year in courseMarksObj) {
-        if (!(year in courseAvgObj)) {
-            courseAvgObj[year] = 0.0;
-        }
-
-        courseAvgObj[year] = courseMarksObj[year].totalMarks / courseMarksObj[year].numberOfCourses;
-        
-        return courseAvgObj;
-    }
-    
-        
-        // if (!(year in courseAvgObj)) {
-        //     courseAvgObj[year] = 0.0;
-        // }
-
-        // courseAvgObj[year] = courseMarksObject[year].totalMarks / courseMarksObject[year].numberOfCourses;
-
-        // return courseAvgObj
-}
-
-const averageSomeCoursesPerYear = (courseRecords: CourseRecords[], courseNames: string[]) => {
-     /**
-     * Return the average marks per year for the given courses.
-     * Only include completes for the year.
-     * @param courseRecords: {CourseRecords[]} array of courseRecord
-     * @param courseNames: {string[]} array of Strings
-     * @returns { string: number } yearTaken to average
-     */
-
-     // Complete this method
-
-
-    if (courseNames.length === 0) 
-        return 0;
-
-    const courseMarksObject: CourseMarks = {};
-    const courseAvgObject: CourseAvg = {};
+const createCourseMarksPerYearObj = (courseRecords: CourseRecords[], courseNames: string[]): CourseMarks => { 
+    const courseMarksPerYearObj: CourseMarks = {};
 
     for (let courseName of courseNames) {
         let courseRecord = courseRecords.find(courseRecord => courseRecord.name === courseName)
@@ -73,26 +36,52 @@ const averageSomeCoursesPerYear = (courseRecords: CourseRecords[], courseNames: 
         let year = courseRecord.yearTaken;
 
         // Check if year exists in obj, if not add it as a new key
-        if (!(year in courseMarksObject)) {
-            courseMarksObject[year] = {
+        if (!(year in courseMarksPerYearObj)) {
+            courseMarksPerYearObj[year] = {
                 totalMarks: 0.0,
                 numberOfCourses: 0
             };
         }
 
-        courseMarksObject[year].totalMarks += courseRecord.mark;
-        courseMarksObject[year].numberOfCourses += 1;
+        courseMarksPerYearObj[year].totalMarks += courseRecord.mark;
+        courseMarksPerYearObj[year].numberOfCourses += 1;
     }
 
-    for (const year in courseMarksObject) {
-        if (!(year in courseAvgObject)) {
-            courseAvgObject[year] = 0.0;
+    return courseMarksPerYearObj;
+}
+
+const createCourseAvgPerYearObj = (courseRecords: CourseRecords[], courseNames: string[]) => { 
+    const courseMarksPerYearObj = createCourseMarksPerYearObj(courseRecords, courseNames);
+    const courseAvgObj: CourseAvg = {};
+
+    for (const year in courseMarksPerYearObj) {
+        if (!(year in courseAvgObj)) {
+            courseAvgObj[year] = 0.0;
         }
 
-        courseAvgObject[year] = courseMarksObject[year].totalMarks / courseMarksObject[year].numberOfCourses;
+        const { totalMarks , numberOfCourses  } = courseMarksPerYearObj[year]
+        courseAvgObj[year] = totalMarks / numberOfCourses;
     }
+
+    return courseAvgObj;
+}
+
+const averageSomeCoursesPerYear = (courseRecords: CourseRecords[], courseNames: string[]) => {
+     /**
+     * Return the average marks per year for the given courses.
+     * Only include completes for the year.
+     * @param courseRecords: {CourseRecords[]} array of courseRecord
+     * @param courseNames: {string[]} array of Strings
+     * @returns { string: number } yearTaken to average
+     */
+
+    // Complete this method
+    if (courseNames.length === 0) 
+        return 0;
+
+    const courseAvgPerYearObj = createCourseAvgPerYearObj(courseRecords, courseNames);
         
-    return courseAvgObject 
+    return courseAvgPerYearObj;
 }
 
 export { CompletionStatus, CourseRecords, averageSomeCoursesPerYear };
